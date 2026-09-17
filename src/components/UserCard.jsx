4dd43b148d951b6596ec532b6f5f42014c73a859
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2026-09-17 14:08:33 +0530

    useState with input

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -2,6 +2,7 @@ import React,{useState} from "react"
 
 const UserCard = (props) => {
     const [count, setCount] = useState(0);
+    const[namee, setNamee] = useState("");
 
     return (
       <div>
@@ -10,7 +11,12 @@ const UserCard = (props) => {
             <p>Experience: {props.experience}</p>
 
             <p>count:{count}</p>
-            <button onClick={() => setCount(count+1)}>Increment</button>
+            {/* <button onClick={() => setCount(count+1)}>Increment</button> */}
+            <button onClick={() => setCount(prev => prev + 1)}>Increment</button>
+            <input type="text" value={namee} onChange={(e) => setNamee(e.target.value)}
+                placeholder="enter name"
+            />
+            <p>{namee}</p>
       </div>
     );
 }
