35767de26a8a4a4ec8d1dda0dba733094bd97d7c
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2026-09-17 14:00:04 +0530

    state

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,9 +1,16 @@
+import React,{useState} from "react"
+
 const UserCard = (props) => {
+    const [count, setCount] = useState(0);
+
     return (
       <div>
             <p>Name: {props.name}</p>
             <p>Role: {props.role}</p>
             <p>Experience: {props.experience}</p>
+
+            <p>count:{count}</p>
+            <button onClick={() => setCount(count+1)}>Increment</button>
       </div>
     );
 }
